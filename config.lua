8cfa9c79507dd86a0bb391b5e24853e80e228f2c
Aaradhya Bhardwaj <87181440+Aaradhya-Bhardwaj@users.noreply.github.com>
2023-03-16 21:03:39 +0530

    Fixed Synatx error in config.lua
    Missing comma at end of line 7 raised an error when start.sh was executed.

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ return {
   font = 'Roboto', -- name of font (it should be installed on your system)
   weather = { -- options for weather module
     api_key = '<API_KEY>', -- from https://openweathermap.org/api
-    city_id = '<CITY_ID>' -- from https://openweathermap.org/api,
+    city_id = '<CITY_ID>', -- from https://openweathermap.org/api,
     unit = '<UNIT_OF_MEASURE>' -- <metric | imperial>
   }
 }
